926c4b6c259b30c48a5fc74da0f41c31b7088156
Lyn <weslley.alves3310@gmail.com>
2025-07-31 20:17:27 -0300

    refactor: renomear classe para seguir padrão de nomenclatura

--- src/main/java/com/example/escola/repository/AlunosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.escola.repository;
-
-import com.example.escola.model.Alunos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunosRepository extends JpaRepository<Alunos, Long> {
-}
