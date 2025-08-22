01512b57f6fd36c8d2c3828864384f6f1d6a4bb2
llyndev <weslley.alves3310@gmail.com>
2025-08-22 19:42:50 -0300

    refactor(AlunoController): ajustar ResponseEntity e adicionar novos endpoints GET e DELETE

--- src/main/java/com/example/escola/controller/AlunoController.java
+++ src/main/java/com/example/escola/controller/AlunoController.java
@@ -1,9 +1,12 @@
 package com.example.escola.controller;
 
 import com.example.escola.dto.AlunoDTO;
+import com.example.escola.dto.AlunoRequest;
 import com.example.escola.model.Aluno;
 import com.example.escola.service.AlunoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,59 +15,90 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/alunos")
+@RequiredArgsConstructor
 public class AlunoController {
 
-    // Injeção de dependencia
+    // Injeção de dependência
     private final AlunoService alunoService;
 
-    public AlunoController(AlunoService alunoService) {
-        this.alunoService = alunoService;
-    }
-
+    // Metodos GET
     @GetMapping
     public List<AlunoDTO> getAll() {
         return alunoService.getAll();
     }
 
-    @GetMapping("/matricula")
-    public ResponseEntity<AlunoDTO> getByid(@RequestParam Long matricula) {
-        return ResponseEntity.ok(alunoService.getByid(matricula));
+    // Metodo GET para receber lista de todos os alunos
+    @GetMapping("/id/{id}")
+    public ResponseEntity<AlunoDTO> getByid(@PathVariable  Long id) {
+        return ResponseEntity.ok(alunoService.getByid(id));
     }
 
+    // Metodo GET para receber alunos de uma determinada serie por nome da serie
+    @GetMapping("/serie/nome")
+    public List<AlunoDTO> getBySerieNome(@RequestParam String serieNome) {
+        return alunoService.getBySerieNome(serieNome);
+    }
+
+    // Metodo GET para receber alunos de uma determinada serie por ID da serie
+    @GetMapping("/serie/id")
+    public List<AlunoDTO> getBySerieId(@RequestParam Long serieId) {
+        return alunoService.getBySerieId(serieId);
+    }
+
+    // Metodo GET para receber aluno pela matricula
+    @GetMapping("/matricula/{matricula}")
+    public ResponseEntity<AlunoDTO> getByMatricula(@PathVariable String matricula) {
+        return ResponseEntity.ok(alunoService.getByMatricula(matricula));
+    }
+
+    // Metodo GET para receber lista de alunos pelo nome
     @GetMapping("/nome")
-    public ResponseEntity<List<AlunoDTO>> getByName(@RequestParam String nome) {
-        List<AlunoDTO> aluno = alunoService.getByName(nome.toUpperCase());
-        return ResponseEntity.ok(aluno);
+    public List<AlunoDTO> getByName(@RequestParam String nome) {
+        return alunoService.getByName(nome);
     }
 
+    // Metodo GET para receber lista de alunos pela data de nascimento
     @GetMapping("/data")
-    public ResponseEntity<List<AlunoDTO>> getByDate(@RequestParam("data") @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate date) {
-        List<AlunoDTO> aluno = alunoService.getByDate(date);
-        return ResponseEntity.ok(aluno);
+    public List<AlunoDTO> getByDate(@RequestParam("data") @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate date) {
+        return alunoService.getByDate(date);
     }
 
-    @GetMapping("/cpf")
-    public ResponseEntity<AlunoDTO> getByCpf(@RequestParam String cpf) {
+    // Metodo GET para receber aluno por CPF
+    @GetMapping("/cpf/{cpf}")
+    public ResponseEntity<AlunoDTO> getByCpf(@PathVariable String cpf) {
         return ResponseEntity.ok(alunoService.getByCpf(cpf));
     }
 
-    @GetMapping("/rg")
-    public ResponseEntity<AlunoDTO> getByRg(@RequestParam String rg) {
+    // Metodo GET para receber aluno por RG
+    @GetMapping("/rg/{rg}")
+    public ResponseEntity<AlunoDTO> getByRg(@PathVariable String rg) {
         return ResponseEntity.ok(alunoService.getByRg(rg));
     }
 
+    // Metodo Post para criar um aluno
     @PostMapping
-    public Aluno create(@RequestBody Aluno aluno) {
-        return alunoService.save(aluno);
+    public ResponseEntity<AlunoDTO> create(@RequestBody AlunoRequest alunoRequest) {
+        AlunoDTO alunoDTO = alunoService.save(alunoRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(alunoDTO);
     }
 
-    @DeleteMapping("/matricula")
-    public void delete(@RequestParam Long matricula) {
-        alunoService.delete(matricula);
+    // Metodo Delete para deletar um aluno pelo ID
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        alunoService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/matricula")
-    public ResponseEntity<Void> update(@RequestParam Long matricula, @RequestBody Aluno alunoDetails) {
+    // Metodo Delete para deltar um aluno pela matricula
+    @DeleteMapping("/matricula/{matricula}")
+    public ResponseEntity<Void> deleteByMatricula(@PathVariable String matricula) {
+        alunoService.deleteByMatricula(matricula);
+        return ResponseEntity.noContent().build();
+    }
+
+    // Metodo Put para atualizar um aluno pela matricula
+    @PutMapping("/matricula/{matricula}")
+    public ResponseEntity<Void> update(@PathVariable String matricula, @RequestBody Aluno alunoDetails) {
         alunoService.update(matricula, alunoDetails);
         return ResponseEntity.ok().build();
     }
