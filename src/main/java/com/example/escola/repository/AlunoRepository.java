86889fbb6ac855d3f23aeca5dcef5df33ea9ce9d
llyndev <weslley.alves3310@gmail.com>
2025-08-22 19:45:47 -0300

    feat(AlunoRepository): adicionar novos métodos

--- src/main/java/com/example/escola/repository/AlunoRepository.java
+++ src/main/java/com/example/escola/repository/AlunoRepository.java
@@ -4,6 +4,7 @@ import com.example.escola.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,9 +14,18 @@ public interface AlunoRepository extends JpaRepository<Aluno, Long> {
 
     List<Aluno> findByNomeContainsIgnoreCase(String nome);
 
+    List<Aluno> findBySerieId(Long serieId);
+
+    List<Aluno> findBySerieNomeContainsIgnoreCase(String serieNome);
+
+    Optional<Aluno> findByMatriculaContainingIgnoreCase(String matricula);
+
     Optional<Aluno> findByCpf(String cpf);
 
     Optional<Aluno> findByRg(String rg);
+
+    @Transactional
+    void deleteByMatricula(String matricula);
 
     @Query("SELECT e FROM Aluno e WHERE e.dataDeNascimento = :data")
     List<Aluno> findByDate(@Param("data") LocalDate date);
