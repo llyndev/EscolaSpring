e7f79b9a023b3dac50f83db2e40fc27c9b735479
llyndev <weslley.alves3310@gmail.com>
2025-08-22 19:44:46 -0300

    refactor(AlunoMapper): adicionar novos parâmetros em toDTO e criar toEntity

--- src/main/java/com/example/escola/mapper/AlunoMapper.java
+++ src/main/java/com/example/escola/mapper/AlunoMapper.java
@@ -1,8 +1,10 @@
 package com.example.escola.mapper;
 
 import com.example.escola.dto.AlunoDTO;
+import com.example.escola.dto.AlunoRequest;
 import com.example.escola.dto.EnderecoDTO;
 import com.example.escola.model.Aluno;
+import com.example.escola.model.Endereco;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,10 +14,14 @@ public class AlunoMapper {
         if (aluno == null) return null;
 
         return  AlunoDTO.builder()
+                .id(aluno.getId())
                 .matricula(aluno.getMatricula())
                 .nome(aluno.getNome())
-                .dataDenascimento(aluno.getDataDeNascimento())
+                .dataDeNascimento(aluno.getDataDeNascimento())
                 .telefone(aluno.getTelefone())
+                .serieNome(aluno.getSerie() != null ? aluno.getSerie().getNome() : null)
+                .serieTurno(aluno.getSerie() != null ? aluno.getSerie().getTurno() : null)
+                .serieSala(aluno.getSerie() != null ? aluno.getSerie().getSala() : null)
                 .endereco(
                         aluno.getEndereco() != null ? new EnderecoDTO(
                                 aluno.getEndereco().getLogradouro(),
@@ -29,5 +35,32 @@ public class AlunoMapper {
                         ) : null
                 )
                 .build();
+    }
+
+    public Aluno toEntity(AlunoRequest alunoRequest) {
+        if (alunoRequest == null) {
+            return null;
+        }
+
+        Aluno aluno = new Aluno();
+        aluno.setMatricula(alunoRequest.getMatricula());
+        aluno.setCpf(alunoRequest.getCpf());
+        aluno.setRg(alunoRequest.getRg());
+        aluno.setNome(alunoRequest.getNome());
+        aluno.setDataDeNascimento(alunoRequest.getDataDeNascimento());
+        aluno.setTelefone(alunoRequest.getTelefone());
+
+        if (alunoRequest.getEndereco() != null) {
+            aluno.setEndereco(new Endereco(
+                    alunoRequest.getEndereco().getLogradouro(),
+                    alunoRequest.getEndereco().getNumero(),
+                    alunoRequest.getEndereco().getComplemento(),
+                    alunoRequest.getEndereco().getBairro(),
+                    alunoRequest.getEndereco().getCidade(),
+                    alunoRequest.getEndereco().getEstado(),
+                    alunoRequest.getEndereco().getCep(),
+                    alunoRequest.getEndereco().getPais()
+            ));
+        } return aluno;
     }
 }
--- src/main/java/com/example/escola/mapper/ProfessorMapper.java
+++ src/main/java/com/example/escola/mapper/ProfessorMapper.java
@@ -2,8 +2,13 @@ package com.example.escola.mapper;
 
 import com.example.escola.dto.EnderecoDTO;
 import com.example.escola.dto.ProfessorDTO;
+import com.example.escola.dto.ProfessorRequest;
+import com.example.escola.model.Disciplina;
+import com.example.escola.model.Endereco;
 import com.example.escola.model.Professor;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
 
 @Component
 public class ProfessorMapper {
@@ -12,11 +17,19 @@ public class ProfessorMapper {
         if (professor == null) return null;
 
         return  ProfessorDTO.builder()
+                .id(professor.getId())
                 .matricula(professor.getMatricula())
                 .nome(professor.getNome())
-                .dataDenascimento(professor.getDataDeNascimento())
-                .licenciatura(professor.getLicenciatura())
+                .dataDeNascimento(professor.getDataDeNascimento())
                 .telefone(professor.getTelefone())
+                .disciplinaNome(
+                        professor.getDisciplinas() != null ?
+                                professor.getDisciplinas()
+                                        .stream()
+                                        .map(Disciplina::getNome)
+                                        .collect(Collectors.toSet())
+                                : null
+                )
                 .endereco(
                         professor.getEndereco() != null ? new EnderecoDTO(
                                 professor.getEndereco().getLogradouro(),
@@ -30,5 +43,32 @@ public class ProfessorMapper {
                         ) : null
                 )
                 .build();
+    }
+
+    public Professor toEntity(ProfessorRequest professorRequest) {
+        if (professorRequest == null) {
+            return null;
+        }
+
+        Professor professor = new Professor();
+        professor.setMatricula(professorRequest.getMatricula());
+        professor.setCpf(professorRequest.getCpf());
+        professor.setRg(professorRequest.getRg());
+        professor.setNome(professorRequest.getNome());
+        professor.setDataDeNascimento(professorRequest.getDataDeNascimento());
+        professor.setTelefone(professorRequest.getTelefone());
+
+        if (professorRequest.getEndereco() != null) {
+            professor.setEndereco(new Endereco(
+                    professorRequest.getEndereco().getLogradouro(),
+                    professorRequest.getEndereco().getNumero(),
+                    professorRequest.getEndereco().getComplemento(),
+                    professorRequest.getEndereco().getBairro(),
+                    professorRequest.getEndereco().getCidade(),
+                    professorRequest.getEndereco().getEstado(),
+                    professorRequest.getEndereco().getCep(),
+                    professorRequest.getEndereco().getPais()
+            ));
+        } return professor;
     }
 }
