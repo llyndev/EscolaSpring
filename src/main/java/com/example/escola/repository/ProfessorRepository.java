aac380ef61c40c182faf3a1e6014c38e2fe2574d
llyndev <weslley.alves3310@gmail.com>
2025-08-22 19:46:01 -0300

    feat(ProfessorRepository): adicionar novos métodos

--- src/main/java/com/example/escola/repository/ProfessorRepository.java
+++ src/main/java/com/example/escola/repository/ProfessorRepository.java
@@ -3,6 +3,7 @@ package com.example.escola.repository;
 import com.example.escola.model.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -12,11 +13,18 @@ public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 
     List<Professor> findByNomeContainingIgnoreCase(String nome);
 
+    List<Professor> findByDisciplinasId(Long id);
+
+    List<Professor> findByDisciplinasNomeContainingIgnoreCase(String disciplinaNome);
+
+    Optional<Professor> findByMatriculaContainingIgnoreCase(String matricula);
+
     Optional<Professor> findByCpf(String cpf);
 
     Optional<Professor> findByRg(String rg);
 
-    List<Professor> findByLicenciaturaContainingIgnoreCase (String licenciatura);
+    @Transactional
+    void deleteByMatriculaContainingIgnoreCase(String matricula);
 
     @Query("SELECT e FROM Professor e WHERE e.dataDeNascimento = :data")
     List<Professor> finByDate(LocalDate date);
